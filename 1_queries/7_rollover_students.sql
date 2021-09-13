603ef388d093ba60122561586f40d7a5cc4a915b
SJ <sj.jega2@gmail.com>
2021-09-13 18:45:20 +0000

    Add student rollover query

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,5 @@
+SELECT students.name AS students_name, cohorts.name AS cohort_name, cohorts.start_date AS cohort_start_date, students.start_date AS student_start_date
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohorts.start_date;
