be40d13cbb473d903c1330bba9bcb53844d34aa5
SJ <sj.jega2@gmail.com>
2021-09-15 22:27:17 +0000

    Install and integrate node-postgress queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,13 +10,16 @@ const pool = new Pool({
 const userInput = process.argv.splice(2);
 
 pool.query(`
-SELECT students.id, students.name, cohorts.name
-FROM students
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${userInput[0]}%'
-LIMIT ${userInput[1]};
+ORDER BY teacher;
 `)
 .then(res => {
   console.log(res.rows);
 })
 .catch(err => console.error('##-->Query Error', err.stack));
+
